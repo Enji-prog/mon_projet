d2d7dfbb90ca8bc41f15afbb1976804bcdc02361
Enji-Prog <isaacengambet@gmail.com>
2026-04-14 14:20:40 +0000

    fix bug

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,9 @@
-from main import addition
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import addition
+
 def test_addition():
- assert addition(2, 3) == 5
+    assert addition(2, 3) == 5
